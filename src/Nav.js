da4e039a02dcda4a5d4830ec523a8c18a8b20a88
AP13 <aguus.lc@hotmail.com>
2022-05-17 12:55:28 -0300

    proyectoReact

---
+++ src/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './header.css'
+import { BsCartFill } from "react-icons/bs";
+import {Link} from 'react-router-dom'
+import { UseCartContext } from './CartContext';
+
+function Nav({links}) {
+    const { IconCart, CartList } = UseCartContext();
+    return (
+        <div className="Menu">
+
+            
+        
+            <ul>
+            <img style={{height:"60px", width:"auto", float:"left", padding:"5px" }} src="/images/logo.png" alt="logo"/>
+                <li><Link to="/">Home</Link></li>
+
+{links.map((link, i) => (<li><Link key={i} to={`/category/${link}`}>{link}</Link></li>))}
+
+<li className="carWidget"><Link style={{padding:"5px 10px"}}to="/cart"><BsCartFill/></Link></li>
+{CartList.length < 1 ? "" : <h2 style={{color:"yellow", padding:"20px 0", float:"right"}}>{IconCart()}</h2>}
+
+            </ul>
+        
+        </div>
+    )
+}
+
+export default Nav
+
